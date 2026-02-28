d6120680c313255783eefa7583c0a9eee924c09e
Laurent M. <laurent@data-shield.eu>
2026-02-28 21:00:42 +0100

    ⚡ [UPDATE] ⚡ License owner

--- install-syswarden-alpine.sh
+++ install-syswarden-alpine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SysWarden for Alpine Linux - Advanced Firewall & Blocklist Orchestrator
-# Copyright (C) 2026 duggytuxy
+# Copyright (C) 2026 duggytuxy - Laurent M.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
