5f260346afe33d67a6bd353649cb0cbbfb205eb5
🔐Laurent M🔐 <laurent@data-shield.eu>
2026-06-11 21:41:40 +0200

    ⚡ 1.00.1 ⚡ : Minor fixes (Please, check release notes for more info)

--- tools/syswarden-waf.sh
+++ tools/syswarden-waf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SysWarden WAF - Enterprise ModSecurity v3 & OWASP CRS Deployment Module
-# Designed for SysWarden v1.00.0 Architecture
+# Designed for SysWarden v1.00.1 Architecture
 # Compatibility: Ubuntu, Debian, RHEL, AlmaLinux, Rocky, CentOS
 
 # --- SAFETY FIRST ---
